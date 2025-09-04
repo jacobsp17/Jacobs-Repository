91166cd4a83763cd623653200cb627f9a1bd4d74
Jacob S. Price <j.s.price9@outlook.com>
2025-09-04 00:53:31 -0400

    Update unsendingMessage.py

--- Projects/unsendingMessage.py
+++ Projects/unsendingMessage.py
@@ -1,12 +1,14 @@
 message = "This is THE secret message!!!"
 
+# write message to the file
 with open('secret_message.txt', 'w') as file:
     file.write(message)
 
+# remove message from the file and notify the user
 with open('secret_message.txt', 'r+') as file:
     original_message = file.read()
     file.seek(0)
-    flag = "this message has been unsent "
+    flag = "this message has been unsent"
     file.write(flag)
     file.truncate(28)
 
